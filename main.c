29e5d87d3910f27d853942549cff06e68519187a
ehtnqls2 <113421745+ehtnqls2@users.noreply.github.com>
2022-10-04 16:40:00 +0900

    02

--- main.c
+++ main.c
@@ -8,14 +8,13 @@ int main(int argc, char *argv[]) {
 	printf("정수 하나를 입력하시오 : ");
 	scanf("%d",&x);
 	
-	if(x>0)
-		printf("양수입니다.");
-		
-	else if(x==0)
-		printf("0입니다.");
+	if(x<0)
+		printf("절댓값은 %d 입니다.",-x);
 		
 	else
-		printf("음수입니다.");
+		printf("절댓값은 %d입니다.",x);
+		
+	
 		 
 	return 0;
 }
