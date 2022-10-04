774468f4a5dbbd7143b6746920e1df317793f04e
ehtnqls2 <113421745+ehtnqls2@users.noreply.github.com>
2022-10-04 17:19:13 +0900

    04

--- main.c
+++ main.c
@@ -4,19 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int cnt=0;
+	int sum=0;
+	int x,cnt;
+	 
+	printf("정수를 입력하세요:");
+	scanf("%d",&x);
 	
-	printf("문자열을 입력하세요: ");
+	for (cnt=0;cnt<=x;cnt++)
+		sum=sum+cnt;
+	 
+	printf("%d",sum);
 	
-	while((c=getchar())!='\n')
-	{
-		if(c>47 && c<58)
-			cnt=cnt+1;
-	 } 
-	
-	printf("숫자의 개수는 %d개입니다.",cnt);
-	
-		 
 	return 0;
 }
