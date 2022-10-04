82c2f94b424db180cf45589f55e3762853dd105b
ehtnqls2 <113421745+ehtnqls2@users.noreply.github.com>
2022-10-04 17:05:48 +0900

    03

--- main.c
+++ main.c
@@ -4,16 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
-	printf("정수 하나를 입력하시오 : ");
-	scanf("%d",&x);
+	char c;
+	int cnt=0;
 	
-	if(x<0)
-		printf("절댓값은 %d 입니다.",-x);
-		
-	else
-		printf("절댓값은 %d입니다.",x);
-		
+	printf("문자열을 입력하세요: ");
+	
+	while((c=getchar())!='\n')
+	{
+		if(c>47 && c<58)
+			cnt=cnt+1;
+	 } 
+	
+	printf("숫자의 개수는 %d개입니다.",cnt);
 	
 		 
 	return 0;
